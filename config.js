4c09dc85ec06cf116ac0e33da90aa516b5f46b8a
MinecraftFreeBot <botmakeing23@gmail.com>
2025-05-25 22:56:52 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const token = 'MTMxMzc5ODgzOTIxMTM5MzAyNQ.GmZ-kZ.grflwShWeZAGrazhjYXiFqV7jZYgPZ4z5t56YE';        // Replace with your bot token
+export const token = 'MTMxMzc5ODgzOTIxMTM5MzAyNQ.Gn0Ktq.ADonvH5CS_ylKnIQQluzV7TGfHFP-MYIlPwjwE';        // Replace with your bot token
 export const clientId = '1313798839211393025';     // Replace with your application ID
 export const guildId = '1289495543709696020';       // Replace with your server (guild) ID
